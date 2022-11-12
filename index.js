f4c025d7da2bc9da4948d466ea7291d44d04546a
Gary Gatchalian <garyandrewgatchalian@gmail.com>
2022-11-13 02:19:46 +0800

    update log/css

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 3001;
 
 //#DUNDIR PATH
 app.use(express.static(path.join(__dirname, 'public')));
+app.set(`views`, path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'images')));
 app.set;
 
 //#EJS
 app.set('view engine', 'ejs');
-app.set(`views`, path.join(__dirname, '/views'));
 
 //GET
 app.get('/', (req, res) => {
